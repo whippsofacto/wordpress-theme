33acd47d09c9c010fb50e604aaa4dcc5def3a0b0
Stephen Whipp <stephen@whippsofacto.com>
2017-07-25 14:49:37 +0100

    017 enqueued script.min.js instead of script.js

--- functions.php
+++ functions.php
@@ -124,7 +124,7 @@ function whippsofacto_scripts() {
 	wp_enqueue_script( 'whippsofacto-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	/* Enqueue my own javascript files*/
-	wp_enqueue_script( 'whippsofacto-script.js', get_template_directory_uri() . '/js/script.js', array(), '20151215', true );
+	wp_enqueue_script( 'whippsofacto-script-min', get_template_directory_uri() . '/js/script.min.js', array(), '20151215', true );
 
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
--- js/script.js
+++ js/script.js
@@ -1,7 +1,1 @@
 console.log("Hello World!");
-var a = 1;
-var b = 2;
-if (a < b){
-  console.log('Hello');
-}
-alert("I'm and alert from my javascript file!");
