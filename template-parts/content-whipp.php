601e8e58ee337b34c38e1856f44dddd41543722b
Stephen Whipp <stephen@whippsofacto.com>
2017-09-09 00:25:51 +0100

    052 Updated the homepage design and improved the headers in most pages that are not projects or home.

--- template-parts/content-whipp.php
+++ template-parts/content-whipp.php
@@ -11,19 +11,12 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header blog-header">
-		<?php if ( !has_post_thumbnail()) {?>
 		 <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-		  <?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
+		  <?php the_title( '<h2 class="entry-title blog-title">', '</h2>' ); ?>
 		 </a>
-		 <?php } ?>
 	</header><!-- .entry-header -->
 	<div class="entry-content blog-content">
 		<?php
-		if ( has_post_thumbnail()) {
-					?><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"> <?php
-					the_post_thumbnail('medium');
-					echo "</a>";
-					}
 					echo '<div class="entry-content">';
 					the_content();
 					echo '</div>';
